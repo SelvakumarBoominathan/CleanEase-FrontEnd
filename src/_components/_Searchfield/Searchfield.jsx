d0b6cb32350e2b0166af20e1fa7cbb37304b7d8b
Selvakumar Boominathan <selvans2k@gmail.com>
2024-06-23 00:41:31 +0530

    added search field

--- src/_components/_Searchfield/Searchfield.jsx
+++ src/_components/_Searchfield/Searchfield.jsx
@@ -5,19 +5,34 @@ import Button from "react-bootstrap/Button";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
+import "./Searchfield-styles.css";
 
 const Searchfield = () => {
   return (
     <div>
       <Navbar className="bg-body-tertiary justify-content-center d-flex flex-column p-5 ">
-        <Card className="text-center">
-          <Card.Body className="text-center">
+        {/* <Card className="text-center"> */}
+        {/* <Card.Body className="text-center">
             <Card.Title>
-              Search for <span className="span">SERVICES!</span>
+              Search for{" "}
+              <span
+                className="Span"
+                style={{ color: "green", fontWeight: "bold" }}
+              >
+                SERVICES
+              </span>
             </Card.Title>
-            <Card.Text>Transforming spaces, one clean at a time</Card.Text>
+            <Card.Text>Transforming spaces, one clean at a time!</Card.Text>
           </Card.Body>
-        </Card>
+        </Card> */}
+
+        <div className="text-field">
+          <h1>
+            Search for <span className="Span">SERVICES</span>
+          </h1>
+          <h6>Transforming spaces, one clean at a time!</h6>
+        </div>
+
         <Form inline>
           <Row className="pt-5 d-flex justify-content-center">
             <Col xs="auto p-2">
@@ -28,7 +43,9 @@ const Searchfield = () => {
               />
             </Col>
             <Col xs="auto p-2">
-              <Button type="submit">Search</Button>
+              <Button type="submit" className="d-flex justify-content-between">
+                <img src="search.svg" />
+              </Button>
             </Col>
           </Row>
         </Form>
