b08a1ccb506683350e5a0a1d334eddd9055ab939
Selvakumar Boominathan <selvans2k@gmail.com>
2024-08-01 22:33:22 +0530

    setting logic to verify otp in varification component

--- src/_components/_OTPvalidation/OTPvalidation.jsx
+++ src/_components/_OTPvalidation/OTPvalidation.jsx
@@ -1,21 +1,30 @@
 import "./OTPvalidation-styles.css";
 import React, { useState } from "react";
-import { Container, Form, Button, Row, Col, Toast } from "react-bootstrap";
+import {
+  Container,
+  Form,
+  Button,
+  Row,
+  Col,
+  Toast,
+  Spinner,
+} from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import { validateOTP } from "../helper";
 
 const OTPvalidation = () => {
   const [otp, setotp] = useState("");
-  const [showToast, setshowToast] = useState(false);
+  const [showAlert, setShowAlert] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
   const navigate = useNavigate();
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     setLoading(true);
     try {
-      setshowToast(true);
-      validateOTP(otp);
+      await validateOTP(otp);
+      setShowAlert(true);
       //to navigate to another component
       setTimeout(() => {
         navigate("/setpassword");
@@ -25,12 +34,30 @@ const OTPvalidation = () => {
         "Error validate OTP:",
         error.response?.data || error.message
       );
+      setError("Incorrect OTP!");
+      setTimeout(() => {
+        setError("");
+      }, 3000);
     } finally {
       setLoading(false);
     }
   };
   return (
     <Container className="d-flex flex-column justify-content-center align-items-center vh-100 w-100">
+      {showAlert && (
+        <div className="alert-overlay">
+          <div className="alert alert-success" role="alert">
+            OTP has been verified!
+          </div>
+        </div>
+      )}
+      {error && (
+        <div className="alert-overlay">
+          <div className="alert alert-danger" role="alert">
+            {error}
+          </div>
+        </div>
+      )}
       <h1>Enter OTP</h1>
       <Row className="fixed-width justify-content-center shadow mt-3 pt-4 pb-4">
         <Col md={12}>
@@ -61,8 +88,8 @@ const OTPvalidation = () => {
             </div>
           </Form>
           <Toast
-            onClose={() => setshowToast(false)}
-            show={showToast}
+            onClose={() => setShowAlert(false)}
+            show={showAlert}
             delay={3000}
             autohide
             className="position-fixed top-0 start-50 translate-middle-x m-3"
