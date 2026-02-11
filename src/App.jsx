0b94becafc357398aaa71c7bf10d743ad0500dde
Selvakumar Boominathan <selvans2k@gmail.com>
2026-02-11 22:11:19 +0530

    removed deadcodes from app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,26 @@
 // import "./App.css";
 import React, { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // Import Routes instead of Switch
-// import Homepage from "./_components/_Homepage.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
-// import Registerpage from "./_components/_Registerpage/Registerpage";
-// import Login from "./_components/_Login/Login";
-// import Emailvarification from "./_components/_Emailvarification/Emailvarification.jsx";
-// import OTPvalidation from "./_components/_OTPvalidation/OTPvalidation.jsx";
-// import Setpassword from "./_components/_Setpassword/Setpassword.jsx";
-// import Bookingpage from "./_components/_Bookingpage/Bookingpage.jsx";
-// import Cartpage from "./_components/_Cart/Cart.jsx";
-// import { Suspense } from "react";
+
 
 // lazy components
-const Homepage = React.lazy(() => import("./_components/_Homepage.jsx"));
-const Registerpage = React.lazy(
-  () => import("./_components/_Registerpage/Registerpage"),
-);
-const Login = React.lazy(() => import("./_components/_Login/Login"));
-const Emailvarification = React.lazy(
+const Homepage = lazy(() => import("./_components/_Homepage.jsx"));
+const Registerpage = lazy(() => import("./_components/_Registerpage/Registerpage.jsx"));
+const Login = lazy(() => import("./_components/_Login/Login.jsx"));
+const Emailvarification = lazy(
   () => import("./_components/_Emailvarification/Emailvarification.jsx"),
 );
-const OTPvalidation = React.lazy(
+const OTPvalidation = lazy(
   () => import("./_components/_OTPvalidation/OTPvalidation.jsx"),
 );
-const Setpassword = React.lazy(
+const Setpassword = lazy(
   () => import("./_components/_Setpassword/Setpassword.jsx"),
 );
-const Bookingpage = React.lazy(
+const Bookingpage = lazy(
   () => import("./_components/_Bookingpage/Bookingpage.jsx"),
 );
-const Cartpage = React.lazy(() => import("./_components/_Cart/Cart.jsx"));
+const Cartpage = lazy(() => import("./_components/_Cart/Cart.jsx"));
 
 function App() {
   return (
